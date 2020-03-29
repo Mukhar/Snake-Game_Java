122240736aff7e2fdc5e08a16056a6a593e528e7
Himanshu <himanshumishra500@gmail.com>
2020-03-29 13:22:23 +0530

    Create Board.java

---
+++ Board.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+public class Board {
+    public final int total_row,total_col;
+    public Cell[][] board;
+    // coordinate for food
+    public int foodx,foody;
+
+    public Board(int row_count,int col_count){
+        // board is initialized here
+        this.foodx=this.foody=0;
+    
+        this.total_col=col_count;
+        this.total_row=row_count;
+
+        this.board=new Cell[row_count][col_count];
+        for (int r=0;r<this.total_row;r++){
+            for (int c=0;r<this.total_col;c++){
+                this.board[r][c]=new Cell(r,c);
+            }    
+        }
+    }
+
+    public void generateFood(){
+        //generating food randomly
+        this.foodx=(int)Math.random()*(this.total_row-1);
+        this.foody=(int)Math.random()*(this.total_col-1);
+
+        this.board[foodx][foody].celltype=CellType.FOOD;
+    }
+
+}
