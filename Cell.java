3bbcaa26416d8b3ea08d2fb1e9f75147ab9bd93e
Himanshu <himanshumishra500@gmail.com>
2020-03-29 12:53:30 +0530

    cell done

---
+++ Cell.java
@@ -0,0 +1,14 @@
+
+
+public class Cell{
+    public int row,col;
+    public CellType celltype;
+    public Cell(int row, int col){
+        this.row=row;
+        this.col=col;
+    }
+
+    public void setCellType(CellType ctype){
+        this.celltype=ctype;
+    }
+}
